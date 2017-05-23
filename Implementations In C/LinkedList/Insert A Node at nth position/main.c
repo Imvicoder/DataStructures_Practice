742e6034d4c02cb99338fa3da97614ebeaf2219d
Vikas Dixit <vikas@skytesters.com>
2017-05-24 00:05:37 +0530

    Insert A Node at Nth Position in LinkedList
    List also printed in forward and reverse order

---
+++ Implementations In C/LinkedList/Insert A Node at nth position/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+int data;
+struct Node* next;
+};
+void insert(int index,int value);
+void print();
+void reversePrint();
+struct Node* head;
+int main()
+{
+    head=NULL;
+    printf("Enter the size of list:");
+    int n;
+    scanf("%d",&n);
+    while(n-->0){
+    printf("Enter position ");
+    int index,value;
+    scanf("%d",&index);
+    printf("Enter value ");
+    scanf("%d",&value);
+    insert(index,value);
+    }
+    print();
+    reversePrint();
+    return 0;
+}
+void insert(int index,int value){
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+    temp->data=value;
+    if(index==0){
+     temp->next=head;
+     head=temp;
+    }else{
+    struct Node* temp1;
+    temp1=head;
+    int i;
+    for(i=0;i<=index-2;i++){
+        temp1=temp1->next;
+    }
+    temp->next=temp1->next; //NUll is passed here to temp->next
+    temp1->next=temp;
+    }
+}
+void print(){
+struct Node* var=head;
+printf("var is:->%d\n",var);
+printf("The Linked List is:->\n");
+while(var!=NULL){
+    printf("%d ",var->data);
+    var=var->next;
+}
+}
+void reversePrint(){
+    struct Node *current,*previous, *nextNode;
+    current=head;
+    previous=NULL;
+    while(current!=NULL){
+    nextNode=current->next;
+    current->next=previous;
+    previous=current;
+    current=nextNode;
+    }
+    head=previous;
+    printf("\nreversed Linked List is:-> ");
+    struct Node* var1=head;
+    while(var1!=NULL){
+        printf("%d ",var1->data);
+        var1=var1->next;
+    }
+}
+
