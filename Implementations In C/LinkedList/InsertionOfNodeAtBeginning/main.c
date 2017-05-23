69a4b428345952a8990eb58e8605cdd84eb95191
Vikas Dixit <vikas@skytesters.com>
2017-05-23 14:37:51 +0530

    Insertion of node at beginning in LinkedList
    Printing Linkedlist in forward and reverse order also implemented

--- Implementations In C/LinkedList/InsertionOfNodeAtBeginning/main.c
+++ Implementations In C/LinkedList/InsertionOfNodeAtBeginning/main.c
@@ -6,6 +6,7 @@ struct Node* next;
 };
 void insert(int x);
 void print();
+void reversePrint();
 struct Node* head; //head is a pointer variable which contains address of first node
 int main()
 {
@@ -14,13 +15,13 @@ int main()
    int n;
    scanf("%d",&n);
    while(n-->0){
-    printf("\nenter the element you wish to insert ");
-    int elt;
-    scanf("%d",&elt);
-    insert(elt);
-
-    print();
+   printf("\nenter the element you wish to insert ");
+   int elt;
+   scanf("%d",&elt);
+   insert(elt);
+   print();
    }
+   reversePrint();
        return 0;
 }
 void insert(int x){
@@ -37,3 +38,23 @@ while(temp1!=NULL){
     temp1=temp1->next;
 }
 }
+void reversePrint(){
+    printf("Reversed List is:->");
+    struct Node* previous;
+    struct Node* NextNode;
+    previous=NULL;
+    struct Node* var=head;
+    while(var!=NULL){
+        NextNode=var->next;
+        var->next=previous;
+        previous=var;
+        var=NextNode;
+    }
+    head=previous;
+    struct Node* temp2=head;
+    while(temp2!=NULL){
+        printf("%d ",temp2->data);
+        temp2=temp2->next;
+    }
+}
+
