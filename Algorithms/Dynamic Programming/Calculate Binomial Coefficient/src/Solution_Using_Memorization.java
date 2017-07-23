43bbd9404d718b1b469a4259da9e62064833d2e8
Vikas Dixit <therisingcoder@gmail.com>
2017-07-23 11:28:10 +0530

    Binomial Coefficient Using Memorization

--- Algorithms/Dynamic Programming/Calculate Binomial Coefficient/src/Solution_Using_Memorization.java
+++ Algorithms/Dynamic Programming/Calculate Binomial Coefficient/src/Solution_Using_Memorization.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Created by Descent-Vikas on 22-07-2017.
  */
 public class Solution_Using_Memorization {
-    public int findCoeffiecient(int n,int r){
+  /*  public int findCoeffiecient(int n,int r){
         int memo[][]=new int[n+1][r+1];
         for(int i=0;i<=n;i++){
             for (int j=0;j<=Math.min(i,r);j++){
@@ -16,12 +16,30 @@ public class Solution_Using_Memorization {
             }
         }
         return memo[n][r];
-    }
+    }*/
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         Solution_Using_Memorization cb=new Solution_Using_Memorization();
         int n = sc.nextInt();
         int r = sc.nextInt();
-        System.out.println(n+"C"+r+" is:->"+cb.findCoeffiecient(n,r));
+        long[][] memo = new long[n+1][r+1];
+
+        // base cases
+        for (int i = 0; i <= n; i++) {
+            memo[i][0] = 1;
+        }
+
+        for (int j = 1; j <= r; j++) {
+            memo[0][j] = 0;
+        }
+
+        // bottom-up dynamic programming
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= r; j++) {
+                memo[i][j] = memo[i-1][j-1] + memo[i-1][j];
+            }
+        }
+
+        System.out.println("Binomial Coefficient is:->"+memo[n][r]);
     }
 }
