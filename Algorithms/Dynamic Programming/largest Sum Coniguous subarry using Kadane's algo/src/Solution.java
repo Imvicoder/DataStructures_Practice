0bee7fba5a48b477caf5b19b7f2c6f46a0c00a9f
Vikas Dixit <therisingcoder@gmail.com>
2017-07-25 12:07:13 +0530

    Largest Sum contigous sub array using Kadane's algo

---
+++ Algorithms/Dynamic Programming/largest Sum Coniguous subarry using Kadane's algo/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution {
+    public int findSum(int [] A,int N){
+        int max_so_far=0;
+        int max_ending_here=0;
+        for(int i=0;i<N;i++){
+            max_ending_here=max_ending_here+A[i];
+            System.out.println("after adding maximum ending here is:->"+max_ending_here);
+            if(max_ending_here<0){
+                max_ending_here=0;
+            }
+            System.out.println("after setting to 0 maximum ending here is:->"+max_ending_here);
+            if(max_so_far<max_ending_here){
+                max_so_far=max_ending_here;
+            }
+            System.out.println(" maximum so far is:->"+max_ending_here);
+        }
+        return max_so_far;
+    }
+    public static void main(String...args){
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int [] ga=new int[N];
+        for(int i=0;i<N;i++){
+            ga[i]=sc.nextInt();
+        }
+        Solution sl=new Solution();
+        System.out.println("maximum Sum for a sub array is:->"+sl.findSum(ga,ga.length));
+    }
+}
